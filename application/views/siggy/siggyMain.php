44ab9e0bc96d068e6d5bb7b48a0d9efe70f64c67
Marek Roszko <mark.roszko@gmail.com>
2013-09-21 22:44:21 -0400

    Added wormhol.es icon

--- application/views/siggy/siggyMain.php
+++ application/views/siggy/siggyMain.php
@@ -16,7 +16,8 @@
         <div id="system-advanced">
             <h2>
                 <span id="system-name"><?php echo !empty($systemName) ? $systemName : 'System'; ?></span>
-                <a href='#' target='_blank' class='dotlan click-me'><img src='public/images/dotlan.png' width='16' height='16'/></a>
+                <a href='#' target='_blank' class='site-icon site-dotlan click-me'><img src='public/images/dotlan.png' width='16' height='16'/></a>
+                <a href='#' target='_blank' class='site-icon site-wormholes click-me'><img src='public/images/wormholes.png' width='16' height='16'/></a>
             </h2>
             <ul class='option-bar tabs'>
                 <li class="active"><a href='#system-info'>Info</a></li>
