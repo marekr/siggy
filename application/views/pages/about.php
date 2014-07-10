38e09d7cdcf5e1599f093542ef735d52edb3fc2b
Marek Roszko <mark.roszko@gmail.com>
2014-07-10 19:36:36 -0400

    Add about page missing from source control

---
+++ application/views/pages/about.php
@@ -0,0 +1,21 @@
+<div class="container">
+	<div class="row">
+		<h2>About siggy</h2>
+		
+		<h3>What is it?</h3>
+		<p>
+			<b>siggy</b> is a wormhole tool that has been in service since 2010. It is avaliable for use by <b>anyone</b> for a small ISK fee.
+		</p>
+		<h3>History</h3>
+		<p>
+			It started out as a lowly signature recording tool <i>visually inspired</i> by another tool called <b>the IGB</b> used by the <b>Demolition Notice</b>. As that alliance fell apart and the corporations split, there was a need among former users for a tool to fill the void. Thus siggy was born. It was much more primitive than it's modern day cousin. Over a few months it came into use by the alliance <b>Talocan United</b> and it's popularity took off from there. 
+		</p>
+		<p>Once upon a time another corporation came along and they were stubborn. They had this awesome visual mapper of chains but it was just boxes, and lines and they basically just drew an ASCII map of their chain ANYWAY regardless of the tool. siggy took upon the challenge and implemented a chain mapping feature. siggy exploded in usage with the arrival of a sane and friendly mapping tool.
+		</p>
+		<p>
+			Over the years, siggy has evolved in appearance and functionality in response to play styles, user opinion and EVE. It hasn't come without a price for development as user feedback can instantly unwind a few days of work in just a hour :)
+		</p>
+		<h3>Development</h3>
+		<p>siggy is closed source and development is done by only 1 individual. If there are any bugs or issues feel free to send a eve mail to <b>Jack Tronic</b>. Feature suggestions welcome.</p>
+	</div>
+</div>
