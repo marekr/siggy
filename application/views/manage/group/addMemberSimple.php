9571d3feba7cd2ed1cd04630113b46f08add76b6
Marek Roszko <mark.roszko@gmail.com>
2014-07-10 23:45:43 -0400

    Updated header for the add member forms

--- application/views/manage/group/addMemberSimple.php
+++ application/views/manage/group/addMemberSimple.php
@@ -17,53 +17,56 @@ $type = array('corp' => 'Corp', 'char' => 'Character');
    <?php if(count($results) > 0 ): ?>
    <h3>Add New Member Search Results</h3>
    <table class="table table-striped">
-		<tr>
-			<th>&nbsp;</th>
-			<th width="80%">Name</th>
-			<th width="10%">Ingame ID</th>
-			<th width="5%">Options</th>
-		</tr>
-		
-		<?php foreach( $results as $result ): ?>
-		<tr>
-			<?php if( $memberType == 'corp' ): ?>
-			<td><img src="http://image.eveonline.com/Corporation/<?php echo $result['corporationID']; ?>_64.png" width="64" height="64" /></td>
-			<td><?php echo $result['corporationName']; ?></td>
-			<td><?php echo $result['corporationID']; ?></td>
-			<td>
-				<form class="form-inline" action="<?php echo URL::base(TRUE,TRUE).'manage/group/addMember/2'; ?>" method="post">
-					<input type="hidden" name="act" value="doForm" />
-					<input type="hidden" name="eveID" value="<?php echo $result['corporationID']; ?>" />
-					<input type="hidden" name="accessName" value="<?php echo $result['corporationName']; ?>" />
-					<input type="hidden" name="memberType" value="<?php echo $memberType; ?>" />
-					<button type="submit" class="btn btn-primary">Select</button>
-				</form>
-			</td>
-			<?php else: ?>
-			<td><img src="http://image.eveonline.com/Character/<?php echo $result['characterID']; ?>_64.jpg" width="64" height="64" /></td>
-			<td><?php echo $result['characterName']; ?></td>
-			<td><?php echo $result['characterID']; ?></td>
-			<td>
-				<form class="form-inline" action="<?php echo URL::base(TRUE,TRUE).'manage/group/addMember/2'; ?>" method="post">
-					<input type="hidden" name="act" value="doForm" />
-					<input type="hidden" name="eveID" value="<?php echo $result['characterID']; ?>" />
-					<input type="hidden" name="accessName" value="<?php echo $result['characterName']; ?>" />
-					<input type="hidden" name="memberType" value="<?php echo $memberType; ?>" />
-					<button type="submit" class="btn btn-primary">Select</button>
-				</form>
-			</td>
-			<?php endif; ?>
-		</tr>
-		<?php endforeach; ?>
+		<thead>
+			<tr>
+				<th>&nbsp;</th>
+				<th width="80%">Name</th>
+				<th width="10%">Ingame ID</th>
+				<th width="5%">Options</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach( $results as $result ): ?>
+			<tr>
+				<?php if( $memberType == 'corp' ): ?>
+				<td><img src="http://image.eveonline.com/Corporation/<?php echo $result['corporationID']; ?>_64.png" width="64" height="64" /></td>
+				<td><?php echo $result['corporationName']; ?></td>
+				<td><?php echo $result['corporationID']; ?></td>
+				<td>
+					<form class="form-inline" action="<?php echo URL::base(TRUE,TRUE).'manage/group/addMember/2'; ?>" method="post">
+						<input type="hidden" name="act" value="doForm" />
+						<input type="hidden" name="eveID" value="<?php echo $result['corporationID']; ?>" />
+						<input type="hidden" name="accessName" value="<?php echo $result['corporationName']; ?>" />
+						<input type="hidden" name="memberType" value="<?php echo $memberType; ?>" />
+						<button type="submit" class="btn btn-primary">Select</button>
+					</form>
+				</td>
+				<?php else: ?>
+				<td><img src="http://image.eveonline.com/Character/<?php echo $result['characterID']; ?>_64.jpg" width="64" height="64" /></td>
+				<td><?php echo $result['characterName']; ?></td>
+				<td><?php echo $result['characterID']; ?></td>
+				<td>
+					<form class="form-inline" action="<?php echo URL::base(TRUE,TRUE).'manage/group/addMember/2'; ?>" method="post">
+						<input type="hidden" name="act" value="doForm" />
+						<input type="hidden" name="eveID" value="<?php echo $result['characterID']; ?>" />
+						<input type="hidden" name="accessName" value="<?php echo $result['characterName']; ?>" />
+						<input type="hidden" name="memberType" value="<?php echo $memberType; ?>" />
+						<button type="submit" class="btn btn-primary">Select</button>
+					</form>
+				</td>
+				<?php endif; ?>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
    </table>
    <?php endif; ?>
 
    <form class="form-horizontal" action="<?php echo URL::base(TRUE,TRUE).'manage/group/addMember/1'; ?>" method="post">
-   <?php if(count($results) > 0 ): ?>
-   <h3>Or search again</h3>
-   <?php else: ?>
-   <h3>Add New Member By Search</h3>
-   <?php endif; ?>
+		<?php if(count($results) > 0 ): ?>
+		<h3>Or search again</h3>
+		<?php else: ?>
+		<h3>Add New Member By Search</h3>
+		<?php endif; ?>
 		<?php echo formRenderer::select('Member Type', 'memberType', $type, 'corp', '', $errors); ?>
 		<?php echo formRenderer::input('Name', 'searchName', '', 'Exact name from EVE ingame for the character or corp', $errors); ?>
 		<div class="form-actions">
--- application/views/template/manage.php
+++ application/views/template/manage.php
@@ -43,8 +43,6 @@
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo URL::base(TRUE, TRUE);?>public/bootstrap/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo URL::base(TRUE, TRUE);?>public/bootstrap/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="<?php echo URL::base(TRUE, TRUE);?>public/bootstrap/ico/apple-touch-icon-57-precomposed.png">
-
-
 </head>
 <body>
 	<nav class="navbar navbar-inverse navbar-fixed-top navbar-siggy" role="navigation" style="margin-bottom: 0">
