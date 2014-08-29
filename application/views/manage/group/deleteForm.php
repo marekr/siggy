03098546c785da1888cf17421707ca4960edd8e0
Marek Roszko <mark.roszko@gmail.com>
2014-08-29 00:33:17 -0400

    Make group member delete button red!

--- application/views/manage/group/deleteForm.php
+++ application/views/manage/group/deleteForm.php
@@ -3,6 +3,6 @@
 <form action="<?php echo URL::base(true,true);?>manage/group/removeMember/<?php echo $id; ?>" method="POST">
 	<input type="hidden" value="1" name="confirm" />
    <p>Are you sure you want to remove '<?php echo $data['accessName']; ?>' from the group access?</p>
-  <button type="submit" class="btn">Confirm Deletion</button>
+  <button type="submit" class="btn btn-danger">Confirm Deletion</button>
   <button type="button" class="btn" onclick="history.go(-1);return false;">Cancel</button>
 </form>
