6bc61c48326a3f48274d186177b5ba9305a53e42
Marek Roszko <mark.roszko@gmail.com>
2014-08-31 00:52:14 -0400

    Add chainmap remove_access view to git.

---
+++ application/views/manage/chainmaps/remove_access.php
@@ -0,0 +1,10 @@
+
+<h2>Confirm deletion</h2>
+<form action="<?php echo URL::base(true,true);?>manage/chainmaps/remove_access/<?php echo $id; ?>" method="POST">
+	<input type="hidden" value="1" name="confirm" />
+	<p>
+		Are you sure you want to remove '<?php echo $data['accessName']; ?>' from the group access?
+	</p>
+	<button type="submit" class="btn btn-danger">Confirm Deletion</button>
+	<button type="button" class="btn" onclick="history.go(-1);return false;">Cancel</button>
+</form>
