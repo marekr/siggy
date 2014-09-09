efe111bb3f5c7fe25ea9c1fb2838cefbf15e8567
Marek Roszko <mark.roszko@gmail.com>
2014-09-08 21:08:15 -0400

    Add jquery hotkeys to thirdparty.compiled.js build. Move all jquery related shit to a jquery subfolder to remove clutter.

--- js.thirdparty.bat
+++ js.thirdparty.bat
@@ -1,17 +1,18 @@
 "C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar compiler.jar ^
---js=jquery-1.11.1.min.js ^
---js=jquery-migrate-core.js ^
---js=jquery-ui-1.10.3.min.js ^
---js=jquery.qtip.js ^
---js=jquery.blockUI.js ^
---js=jquery.color.js ^
---js=jquery.autocomplete.js ^
---js=jquery.tablesorter.js ^
---js=jquery.flot.js ^
---js=jquery.contextMenu.js ^
---js=jsBezier-0.6-min.js ^
---js=jquery.jsPlumb-1.5.5-min.js ^
---js=jquery.placeholder.js ^
+--js=jquery/jquery-1.11.1.min.js ^
+--js=jquery/jquery-migrate-core.js ^
+--js=jquery/jquery-ui-1.10.3.min.js ^
+--js=jquery/jquery.qtip.js ^
+--js=jquery/jquery.blockUI.js ^
+--js=jquery/jquery.color.js ^
+--js=jquery/jquery.autocomplete.js ^
+--js=jquery/jquery.tablesorter.js ^
+--js=jquery/jquery.flot.js ^
+--js=jquery/jquery.contextMenu.js ^
+--js=jquery/jquery.hotkeys.js ^
+--js=jquery/jsBezier-0.6-min.js ^
+--js=jquery/jquery.jsPlumb-1.5.5-min.js ^
+--js=jquery/jquery.placeholder.js ^
 --js=dropdown.js ^
 --js_output_file=thirdparty.compiled.js
 @pause
