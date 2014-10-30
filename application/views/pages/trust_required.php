4143cc012d2d6c72257859c2797c65b775e8d07c
Marek Roszko <mark.roszko@gmail.com>
2014-10-29 23:12:06 -0400

    Update trust required page to use bootstrap 3.2 properly.

--- application/views/pages/trust_required.php
+++ application/views/pages/trust_required.php
@@ -5,10 +5,14 @@ CCPEVE.requestTrust('<?php echo URL::base(true,true); ?>*');
 }
 </script>
 
-<div class="well">
-	<h2>Welcome to siggy!</h2>
-	<p>Please accept the trust request and click the button to continue:</p>
-	<div class="text-centered">
-		<a href="<?php echo URL::base(true,true); ?>" class="btn btn-primary btn-large">Continue</a>
+<div class="container">
+	<div class="row">
+		<div class="well">
+			<h2>Welcome to siggy!</h2>
+			<p>
+				Please accept the trust request and click the button to continue
+				<a href="<?php echo URL::base(true,true); ?>" class="btn btn-primary btn-large">Continue</a>
+			</p>
+		</div>
 	</div>
 </div>
