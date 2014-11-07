9faf9362fabd9bac37991986617ee7bf51ce0642
Marek Roszko <mark.roszko@gmail.com>
2014-11-06 21:17:08 -0500

    Update password reset complete page to latest bootstrap.

--- application/views/messages/passwordResetCompleteMessage.php
+++ application/views/messages/passwordResetCompleteMessage.php
@@ -1,7 +1,6 @@
-
-    <div id="message" style='width:450px;'>
-					<h1>Password Reset Complete</h1>
-					<div id="authBox">
-							<p>Your account's password has been reset. An email has been dispatched with a temporary password you may use to login and then change it to anything you wish.</p>
-          </div>
+<div class="container">
+    <div class="row">
+		<h2>Password Reset Complete</h2>
+		<p>Your account's password has been reset. An email has been dispatched with a temporary password you may use to login and then change it to anything you wish.</p>
     </div>
+</div>
