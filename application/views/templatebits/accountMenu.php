a2a20469294221ccdbabd9a8b5c304671a3b389b
Marek Roszko <mark.roszko@gmail.com>
2014-11-15 22:23:52 -0500

    Fix bad header tag.

--- application/views/templatebits/accountMenu.php
+++ application/views/templatebits/accountMenu.php
@@ -1,5 +1,5 @@
 			
-	<h4 class="nav-header">Account Management</h5>
+	<h4 class="nav-header">Account Management</h4>
 	<ul class="nav nav-pills nav-stacked">
 		<li><a href="<?php echo URL::base(TRUE, TRUE);?>account/overview">Overview</a></li>
 		<li><a href="<?php echo URL::base(TRUE, TRUE);?>account/changePassword">Change Password</a></li>
