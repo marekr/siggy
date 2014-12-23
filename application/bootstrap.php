0d889d2c3d4a899d07eb9958a4a89aa99073517c
Marek Roszko <mark.roszko@gmail.com>
2014-12-22 22:59:42 -0500

    Bump version.

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -131,7 +131,7 @@ if( Kohana::$environment == Kohana::DEVELOPMENT && strpos($_SERVER['HTTP_USER_AG
   $_SERVER['HTTP_EVE_SOLARSYSTEMID'] = 31002019;
 	}
 }*/
-define('SIGGY_VERSION', '2.16.0b');
+define('SIGGY_VERSION', '2.16.1');
 if( Kohana::$environment == Kohana::PRODUCTION)
 {
 	define('WIN_DEV', true);
