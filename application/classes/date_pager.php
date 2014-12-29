33d97517432a7368f059ca69ffb1c8f62bbd180c
Marek Roszko <mark.roszko@gmail.com>
2014-12-28 22:17:01 -0500

    Update date pager with daily functionality. Fix week timestamping, use ISO year to avoid week 52/53 issue rolling over to week 1.

--- application/classes/date_pager.php
+++ application/classes/date_pager.php
@@ -17,10 +17,10 @@ class Date_Pager
 	private $nextDay = 0;
 	private $nextYear = 0;
 	
-    const MODEWEEKLY = 0;
-    const MODEMONTHLY = 1;
-    const MODEDAILY = 2;
-    const MODEYEARLY = 3;
+	const MODEWEEKLY = 0;
+	const MODEMONTHLY = 1;
+	const MODEDAILY = 2;
+	const MODEYEARLY = 3;
 	
 	public $mode = self::MODEWEEKLY;
 
@@ -34,7 +34,7 @@ class Date_Pager
 		}
 		else
 		{
-			$this->year = date("Y");
+			$this->year = date("o");
 		}
 		
 		if( $this->mode == self::MODEWEEKLY )
@@ -48,27 +48,15 @@ class Date_Pager
 				$this->week = date("W");
 			}
 			
-			$this->prevWeek = $this->week - 1;
-			if( $this->prevWeek < 1 )
-			{
-				$this->prevYear = $this->year -1;
-				$this->prevWeek = 53;
-			}
-			else
-			{
-				$this->prevYear = $this->year;
-			}
+			$now = strtotime($this->year."W".$this->week);
 			
-			$this->nextWeek = $this->week + 1;
-			if( $this->nextWeek > 53 )
-			{
-				$this->nextWeek = 1;
-				$this->nextYear = $this->year + 1;
-			}
-			else
-			{
-				$this->nextYear = $this->year;
-			}
+			$prevDate = strtotime('-1 week',$now);
+			$this->prevWeek = date("W",$prevDate);
+			$this->prevYear = date("o",$prevDate);
+			
+			$nextDate = strtotime('+1 week',$now);
+			$this->nextWeek = date("W",$nextDate);
+			$this->nextYear = date("o",$nextDate);
 		}
 		else if( $this->mode == self::MODEMONTHLY )
 		{
@@ -108,6 +96,23 @@ class Date_Pager
 			$this->prevYear = $this->year -1;
 			$this->nextYear = $this->year + 1;
 		}
+		else if( $this->mode == self::MODEDAILY )
+		{
+			$this->month = $month;
+			$this->day = $day;
+
+			$now = strtotime($this->month.'/'.$this->day.'/'.$this->year);
+
+			$prev = getdate(strtotime('-1 day',$now));
+			$this->prevDay = $prev['mday'];
+			$this->prevMonth = $prev['mon'];
+			$this->prevYear = $prev['year'];
+
+			$next =  getdate(strtotime('+1 day',$now));
+			$this->nextDay = $next['mday'];
+			$this->nextMonth = $next['mon'];
+			$this->nextYear = $next['year'];
+		}
 	}
 	
 	public function getTimestamps()
@@ -127,8 +132,23 @@ class Date_Pager
 		{
 			list($start, $end) = $this->yearTimestamps( $this->year );
 		}
+		else if( $this->mode == self::MODEDAILY )
+		{
+			list($start, $end) = $this->dayTimestamps( $this->day, $this->month, $this->year );
+		}
 		
 		return array( 'start' => $start, 'end' => $end );
+	}
+
+	private function dayTimestamps($day, $month, $year)
+	{
+		$now = strtotime($month.'/'.$day.'/'.$year);
+
+		$prev = strtotime("midnight", $now);
+
+		$next = strtotime("tomorrow", $now) - 1;
+
+		return array( $prev, $next );
 	}
 	
 	private function monthTimestamps($month, $year)
@@ -168,6 +188,10 @@ class Date_Pager
 		{
 			return array ('text' => $this->prevYear, 'urlbit' => 'year/' . $this->prevYear);
 		}
+		else if( $this->mode == self::MODEDAILY )
+		{
+			return array ('text' => $this->getMonthName( $this->prevMonth ) . ' ' . $this->prevDay . ', ' . $this->prevYear, 'urlbit' => 'year/' . $this->prevYear . '/month/' . $this->prevMonth . '/day/' . $this->prevDay);
+		}
 	}
 	
 	public function getCurrentDate()
@@ -184,6 +208,10 @@ class Date_Pager
 		{
 			return array ('text' => $this->year, 'urlbit' => 'year/' . $this->year);
 		}
+		else if( $this->mode == self::MODEDAILY )
+		{
+			return array ('text' => $this->getMonthName( $this->month ) . ' ' . $this->day . ', ' . $this->year, 'urlbit' => 'year/' . $this->year . '/month/' . $this->month . '/day/' . $this->day);
+		}
 	}
 	
 	public function getNextDate()
@@ -199,6 +227,10 @@ class Date_Pager
 		else if( $this->mode == self::MODEYEARLY )
 		{
 			return array ('text' => $this->nextYear, 'urlbit' => 'year/' . $this->nextYear);
+		}
+		else if( $this->mode == self::MODEDAILY )
+		{
+			return array ('text' => $this->getMonthName( $this->nextMonth ) . ' ' . $this->nextDay . ', ' . $this->nextYear, 'urlbit' => 'year/' . $this->nextYear . '/month/' . $this->nextMonth . '/day/' . $this->nextDay);
 		}
 	}
 	
