cae82a313050bfa50ffacbedffb3959ac0592c93
Marek Roszko <mark.roszko@gmail.com>
2015-01-05 23:44:34 -0500

    Add group notes template to git

---
+++ application/views/siggy/boxes/group_notes.php
@@ -0,0 +1,14 @@
+
+<div id="global-notes" class="box" style="display:none;">
+	<h3>Notes <a href="#" id="global-notes-edit" class="btn btn-xs btn-primary">edit</a></h3>
+	<div>
+		<textarea style="width:100%;height:100px;display:none;" id="global-notes-edit-box">
+		</textarea>
+		<div id="global-notes-content">No notes loaded.</div>
+		<div class="center">
+			<button style="display:none" id="global-notes-save" class="btn btn-default btn-xs">Save</button>
+			<button style="display:none" id="global-notes-cancel" class="btn btn-default btn-xs">Cancel</button><br />
+			<p>Last update: <span id="global-notes-time">00:00:00</span></p>
+		</div>
+	</div>
+</div>
