83821a5650570f0f8a734187eed5490b8492cce3
Marek Roszko <mark.roszko@gmail.com>
2015-01-07 23:44:41 -0500

    Add composer and use it :D

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -207,6 +207,9 @@ else
 {
 	Database::$default = 'default';
 }
+
+require APPPATH . 'vendor/autoload.php';
+
 /**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
