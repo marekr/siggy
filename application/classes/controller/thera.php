ea0b1fb1ba37bbb80a0bb7d447d4ce685e040ed7
Marek Roszko <mark.roszko@gmail.com>
2015-01-08 23:45:31 -0500

    Cache the output of the controller rather than just the fetching of json data. Also fix formatting.

--- application/classes/controller/thera.php
+++ application/classes/controller/thera.php
@@ -18,8 +18,8 @@ class Controller_Thera extends FrontController
 	{
 		parent::before();
 	}
-	
-	
+
+
 	public function action_latest_exits()
 	{
 		$this->profiler = NULL;
@@ -27,76 +27,79 @@ class Controller_Thera extends FrontController
 		header('content-type: application/json');
 		header("Cache-Control: no-cache, must-revalidate");
 
-		
 		$cache = Cache::instance( CACHE_METHOD );
 
-		$cache_name = 'thera-exits-evescout';;
+		$cache_name = 'thera-exits';
 
-		if( ($data = $cache->get( $cache_name, FALSE )) == FALSE )
+		if( ($exits = $cache->get( $cache_name, FALSE )) == FALSE )
 		{
 			$data = $this->fetch_thera_json();
 
-		
-			if( $data != null )
+			$exits = $this->build_exits_data($data);
+
+			if( $exits != null )
 			{
-				$cache->set($cache_name, $data, 300);
+				$cache->set($cache_name, $exits, 300);
 			}
 		}
-		
-		
-		$wormholes = DB::query(Database::SELECT, 'SELECT id, name
-													FROM statics')
-						->execute()->as_array('name');
-		
+
+		print json_encode($exits);
+		exit();
+	}
+
+	private function build_exits_data($data)
+	{
+		$wormholes = DB::query(Database::SELECT, 'SELECT id, name FROM statics')
+								->execute()->as_array('name');
+
 		$exits = array();
 		foreach( $data as $rawExit )
 		{
-			
-			$exit =  array( 	'id' => $rawExit->id,
-								'created_at' => Carbon::parse($rawExit->createdAt)->timestamp,
-								'wormhole_type' => '',
-								'out_signature' => $rawExit->signatureId,
-								'in_signature' => $rawExit->wormholeDestinationSignatureId
-								);
-								
-								
-			$system = DB::query(Database::SELECT, 'SELECT s.id, s.name, s.sysClass as sys_class, s.region as region_id, r.regionName as region_name, s.sec
-														FROM solarsystems  s
-														INNER JOIN regions r ON(s.region=r.regionID)
-														WHERE s.id=:system
-														')
-							->param(':system', $rawExit->wormholeDestinationSolarSystemId)
-							->execute()->current();
-						
-								
+			$exit =  array( 'id' => $rawExit->id,
+							'created_at' => Carbon::parse($rawExit->createdAt)->timestamp,
+							'wormhole_type' => '',
+							'out_signature' => $rawExit->signatureId,
+							'in_signature' => $rawExit->wormholeDestinationSignatureId
+						);
+
+
+			$system = DB::query(Database::SELECT, 'SELECT s.id, s.name,
+													s.sysClass as sys_class,
+													s.region as region_id,
+													r.regionName as region_name, s.sec
+													FROM solarsystems  s
+													INNER JOIN regions r ON(s.region=r.regionID)
+													WHERE s.id=:system')
+				->param(':system', $rawExit->wormholeDestinationSolarSystemId)
+				->execute()
+				->current();
+
+
 			$wormholeType = '';
 			if( $rawExit->sourceWormholeType->id != 91 )
 			{
-				$wormholeType = $rawExit->sourceWormholeType->name; 
+				$wormholeType = $rawExit->sourceWormholeType->name;
 			}
 			else
 			{
-				$wormholeType = $rawExit->destinationWormholeType->name; 
+				$wormholeType = $rawExit->destinationWormholeType->name;
 			}
-			
+
 			$exit['wormhole_type'] = (int)$wormholes[ $wormholeType ]['id'];
-			
+
 			$system['id'] = (int)$system['id'];
 			$system['sys_class'] = (int)$system['sys_class'];
 			$system['region_id'] = (int)$system['region_id'];
 			$system['security'] = (float)$system['sec'];
 			$exit['system'] = $system;
-			
-			
+
+
 			$exits[$rawExit->id] = $exit;
-								
-			
 		}
-		
-		print json_encode($exits);
-		exit();
+
+		return $exits;
 	}
-	
+
 	private function fetch_thera_json()
 	{
 		$opts = array(
@@ -111,12 +114,12 @@ class Controller_Thera extends FrontController
 		$context = stream_context_create($opts);
 
 		$data = file_get_contents('http://www.eve-scout.com/api/wormholes?limit=1000&offset=0&sort=wormholeEstimatedEol&order=asc', false, $context);
-		
+
 		if( !empty($data) )
 		{
 			$data = json_decode($data);
 		}
-			
+
 		return $data;
 	}
 }
