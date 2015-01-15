d5663caddb893366990020c9957d423f29bd4c44
Marek Roszko <mark.roszko@gmail.com>
2015-01-14 19:51:11 -0500

    Eliminate the never used apple icons

--- application/views/template/public_bootstrap32.php
+++ application/views/template/public_bootstrap32.php
@@ -23,10 +23,6 @@
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="<?php echo URL::base(TRUE, TRUE);?>favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo URL::base(TRUE, TRUE);?>public/bootstrap/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo URL::base(TRUE, TRUE);?>public/bootstrap/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo URL::base(TRUE, TRUE);?>public/bootstrap/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="<?php echo URL::base(TRUE, TRUE);?>public/bootstrap/ico/apple-touch-icon-57-precomposed.png">
 
 	<script type="text/javascript">
 	$(document).ready(function() {
