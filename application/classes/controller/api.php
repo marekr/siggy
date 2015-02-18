4d0cb432c3f7ab1c3c36ce9c2517b1c8959d65b2
Marek Roszko <mark.roszko@gmail.com>
2015-02-17 23:01:59 -0500

    Eliminate old method from old api Set parent class to REST

--- application/classes/controller/api.php
+++ application/classes/controller/api.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Api extends Controller
+class Controller_Api extends Controller_REST	
 {
 
 	private $apiErrors = array();
@@ -46,58 +46,6 @@ class Controller_Api extends Controller
 		}
 		else
 		{
-			return false;
-		}
-	}
-	
-	public function action_systemsList()
-	{
-		$results = array();
-		
-		if( $this->apiKey['subGroupID'] != -1 )
-		{
-			$mapData = $this->__getChainMapSystemData( $this->apiKey['groupID'], $this->apiKey['subGroupID'] );
-			
-			$results['groupID'] = $this->apiKey['groupID'];
-			
-			$results['subGroups'][ intval($this->apiKey['subGroupID']) ]['subGroupID'] = $this->apiKey['subGroupID'];
-			foreach( $mapData['systems'] as $system )
-			{
-				$sys['systemID'] = $system['systemID'];
-				$sys['name'] = $system['name'];
-				$sys['displayName'] = $system['displayName'];
-				$sys['activity'] = $system['activity']; 
-		
-				$results['subGroups'][ intval($this->apiKey['subGroupID']) ]['systems'][] = $sys;
-			}	
-		}
-		
-		if( !count($this->apiErrors) )
-		{
-			$output = array( 'status' => 'success', 'time' => time(), 'results' => $results );
-		}
-		else
-		{
-			$output = array('status' => 'failed', 'time' => time(), 'errors' => $this->apiErrors);
-		}
-		
-		echo json_encode($output);
-		exit();
-	}
-	
-	private function __getChainMapSystemData( $groupID, $subGroupID )
-	{
-		$cache = Cache::instance();
-		
-		$cacheName = 'mapCache-'.$groupID.'-'.$subGroupID;
-		
-		if( $mapData = $cache->get( $cacheName, FALSE ) )
-		{
-			return $mapData;
-		}
-		else
-		{
-			$this->apiErrors[] = 'Error: Map data cache for group/subgroup combination does not exist';
 			return false;
 		}
 	}
