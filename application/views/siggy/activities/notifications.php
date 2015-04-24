ebf703ed6321c9e3eb6cfe0645159e2a86721f74
Marek Roszko <mark.roszko@gmail.com>
2015-04-23 20:04:21 -0400

    Add missing view files

---
+++ application/views/siggy/activities/notifications.php
@@ -0,0 +1,29 @@
+
+<div id="activity-notifications" class="wrapper" style="display:none">
+	<p>
+		Hello
+	</p>
+	<h4>Settings</h4>
+	<table class="siggy-table siggy-table-striped table-with-dropdowns" id="notifications-settings-table">
+		<thead>
+			<tr>
+				<th>Type</th>
+				<th>Action</th>
+				<th>&nbsp;</th>
+			</tr>
+		</thead>
+		<tbody>
+		</tbody>
+	</table>
+	<h4>History</h4>
+	<table class="siggy-table siggy-table-striped table-with-dropdowns" id="notifications-history-table">
+		<thead>
+			<tr>
+				<th width='10%'>Date</th>
+				<th>Message</th>
+			</tr>
+		</thead>
+		<tbody>
+		</tbody>
+	</table>
+</div>
