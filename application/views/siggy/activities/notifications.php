018e1ae49ddc04770c423ae6b7ea16c170b27b9c
Marek Roszko <mark.roszko@gmail.com>
2015-05-04 01:58:04 -0400

    Add tabs to notification activity

--- application/views/siggy/activities/notifications.php
+++ application/views/siggy/activities/notifications.php
@@ -1,37 +1,55 @@
 
+
 <div id="activity-notifications" class="wrapper" style="display:none">
-	<p>
-		<div class="dropdown pull-right">
-			<button id="activity-notification-add" class="btn btn-primary" type="button" data-toggle="dropdown" aria-expanded="true">
-				New Notifier <span class="caret"></span>
-			</button>
-			<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-				<li role="presentation"><a role="menuitem" id='notifier_add_system_mapped' tabindex="-1" href="#">System mapped</a></li>
-				<li role="presentation"><a role="menuitem" id='notifier_add_resident_found'tabindex="-1" href="#">Resident found</a></li>
-			</ul>
+	<div class='clearfix'></div>
+	<ul class="box-tabs">
+		<li role='presentation' class='active'>
+			<a href='#notification-history' aria-controls='home' role='tab' data-toggle='tab'>
+				History
+			</a>
+		</li>
+		<li role='presentation'>
+			<a href='#notification-notifiers' aria-controls='home' role='tab' data-toggle='tab'>
+				Settings
+			</a>
+		</li>
+	</ul>
+	<div class='tab-content'>
+		<div role="tabpanel" id="notification-notifiers" class="box-tab tab-pane">
+			<p>
+				<div class="dropdown pull-right">
+					<button id="activity-notification-add" class="btn btn-primary" type="button" data-toggle="dropdown" aria-expanded="true">
+						New Notifier <span class="caret"></span>
+					</button>
+					<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+						<li role="presentation"><a role="menuitem" id='notifier_add_system_mapped' tabindex="-1" href="#">System mapped</a></li>
+						<li role="presentation"><a role="menuitem" id='notifier_add_resident_found'tabindex="-1" href="#">Resident found</a></li>
+					</ul>
+				</div>
+			</p>
+			<table class="siggy-table siggy-table-striped table-with-dropdowns" id="notifications-settings-table">
+				<thead>
+					<tr>
+						<th>Type</th>
+						<th>Action</th>
+						<th>&nbsp;</th>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
 		</div>
-	</p>
-	<h4>Settings</h4>
-	<table class="siggy-table siggy-table-striped table-with-dropdowns" id="notifications-settings-table">
-		<thead>
-			<tr>
-				<th>Type</th>
-				<th>Action</th>
-				<th>&nbsp;</th>
-			</tr>
-		</thead>
-		<tbody>
-		</tbody>
-	</table>
-	<h4>History</h4>
-	<table class="siggy-table siggy-table-striped table-with-dropdowns" id="notifications-history-table">
-		<thead>
-			<tr>
-				<th width='10%'>Date</th>
-				<th>Message</th>
-			</tr>
-		</thead>
-		<tbody>
-		</tbody>
-	</table>
+		<div role="tabpanel" id="notification-history" class="box-tab tab-pane active">
+			<table class="siggy-table siggy-table-striped table-with-dropdowns" id="notifications-history-table">
+				<thead>
+					<tr>
+						<th width='10%'>Date</th>
+						<th>Message</th>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
