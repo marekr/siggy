d7e69fb74856289c009557dbfaa41847f6ecfd7e
Marek Roszko <mark.roszko@gmail.com>
2015-05-20 01:38:33 -0400

    Use memcache for API data

--- application/classes/PhealHelper.php
+++ application/classes/PhealHelper.php
@@ -6,8 +6,16 @@ class PhealHelper {
 	
 	public static function configure()
 	{
-		Config::getInstance()->cache = new \Pheal\Cache\FileStorage(APPPATH.'cache/api/');
 		Config::getInstance()->http_ssl_verifypeer = false;
-		Config::getInstance()->http_user_agent = 'siggy '.SIGGY_VERSION.' borkedlabs@gmail.com';
+		Config::getInstance()->http_user_agent = 'siggy '.SIGGY_VERSION.' mark.roszko@gmail.com';
+		
+		if(CACHE_METHOD == 'memcache')
+		{
+			Config::getInstance()->cache = new \Pheal\Cache\MemcacheStorage( array('host' => 'localhost', 'port' => 11211) );
+		}
+		else
+		{
+			Config::getInstance()->cache = new \Pheal\Cache\FileStorage(APPPATH.'cache/api/');
+		}
 	}
 }
