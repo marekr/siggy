f517bad192eb8248ebab6cd715dbab12605f7c5c
Marek Roszko <mark.roszko@gmail.com>
2016-10-08 09:32:17 -0400

    Get rid of more apple favicon stuff

--- application/views/errors/general.php
+++ application/views/errors/general.php
@@ -22,11 +22,6 @@
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="<?php echo URL::base(TRUE, TRUE);?>favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo URL::base(TRUE, TRUE);?>bootstrap/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo URL::base(TRUE, TRUE);?>bootstrap/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo URL::base(TRUE, TRUE);?>bootstrap/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="<?php echo URL::base(TRUE, TRUE);?>bootstrap/ico/apple-touch-icon-57-precomposed.png">
-
 	<script type="text/javascript">
 	$(document).ready(function() {
 		$('.dropdown-toggle').dropdown()
--- application/views/template/public.php
+++ application/views/template/public.php
@@ -16,12 +16,6 @@
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="<?php echo URL::base(TRUE, TRUE);?>favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo URL::base(TRUE, TRUE);?>bootstrap/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo URL::base(TRUE, TRUE);?>bootstrap/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo URL::base(TRUE, TRUE);?>bootstrap/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="<?php echo URL::base(TRUE, TRUE);?>bootstrap/ico/apple-touch-icon-57-precomposed.png">
-
-
 </head>
 <body data-spy="scroll">
     <div class="navbar navbar-inverse navbar-fixed-top">
