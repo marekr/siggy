58989e42fc16ec03aca96839f04b2179242c2c0b
Marek Roszko <mark.roszko@gmail.com>
2017-01-12 00:09:32 -0500

    Bump version to update js

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -111,7 +111,7 @@ if (isset($_SERVER['SERVER_PROTOCOL']))
 
 Kohana::$environment = ($_SERVER['SERVER_NAME'] !== 'localhost' && $_SERVER['SERVER_NAME'] !== 'dev.siggy.borkedlabs.com') ? Kohana::PRODUCTION : Kohana::DEVELOPMENT;
 
-define('SIGGY_VERSION', '2.29.0a');
+define('SIGGY_VERSION', '2.29.1');
 if( Kohana::$environment == Kohana::PRODUCTION)
 {
 	define('WIN_DEV', true);
