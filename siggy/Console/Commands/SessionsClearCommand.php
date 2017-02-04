5504f91f54e1f7f4c41a89ef7aa91c802875f862
Marek Roszko <mark.roszko@gmail.com>
2017-02-04 00:05:51 -0500

    Add "artisan" for cron handling.

---
+++ siggy/Console/Commands/SessionsClearCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Siggy\Console\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use Illuminate\Database\Capsule\Manager as DB;
+
+class SessionsClearCommand extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'sessions:clear';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Clear old sesssions';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		//30 minute cutoff
+		$cutoff = Carbon::now()->subMinutes(30)->toDateTimeString();
+
+		DB::table('sessions')
+			->where('updated_at', '<=', $cutoff)
+			->delete();
+
+		$this->info('Deleted old sessions');
+	}
+}
