83d6ada1ca8317be15d08ed4b477b1248156df6f
Marek Roszko <mark.roszko@gmail.com>
2017-02-07 00:44:41 -0500

    Add structures stub

---
+++ application/views/siggy/displaygroups/structures.php
@@ -0,0 +1,20 @@
+<div id="structure-box" class="sub-display-group">
+	<div class='sub-display-group-header hover'>Structures<i class="expand-collapse-indicator fa fa-caret-down pull-right fa-lg"></i></div>
+	<div class='sub-display-group-content'>
+		<a href="#" id='system-intel-add-structure' class="btn btn-default btn-xs">Add Structure</a><br /><br />
+		<table id="system-intel-structures" cellspacing="1" class='siggy-table bordered-wrap'>
+			<thead>
+				<tr>
+					<th width="4%">Status</th>
+					<th width="36%">Corp</th>
+					<th width="4%">Type</th>
+					<th width="8%">Added</th>
+					<th width="32%">Notes</th>
+					<th width="8%">&nbsp;</th>
+				</tr>
+			</thead>
+			<tbody>
+			</tbody>
+		</table>
+	</div>
+</div>
