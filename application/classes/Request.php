1bc96240c160d85a7d8c0af68e613a4aa17f091d
Marek Roszko <mark.roszko@gmail.com>
2017-02-18 15:18:12 -0500

    Add missing proxy to trusted list

--- application/classes/Request.php
+++ application/classes/Request.php
@@ -5,7 +5,8 @@ class Request extends Kohana_Request {
 	public static $trusted_proxies = array('127.0.0.1',
 											'localhost',
 											'localhost.localdomain',
-											'104.131.58.157',
+											'104.131.58.157',	//proxy 1
+											'159.203.193.36',	//proxy 2
 											'dev.siggy.borkedlabs.com'
 											);
 }
