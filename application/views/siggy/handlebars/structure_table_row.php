4662c2fb9c5999c6a1591a7a2024e34dbff02774
Marek Roszko <mark.roszko@gmail.com>
2017-02-25 20:01:29 -0500

    Shrink input sizes, remove border radius

--- application/views/siggy/handlebars/structure_table_row.php
+++ application/views/siggy/handlebars/structure_table_row.php
@@ -1,21 +1,20 @@
-
-	<script id="template-structure-table-row" type="text/x-handlebars-template">?
-		<tr id='structure-{{ id }}' data-id='{{ id }}'>
-			<td class='text-center'>
-				{{ corporation_name }}
-			</td>
-			<td>
-				{{structureTypeName type_id}}
-			</td>
-			<td class='text-center'>
-				{{ created_at }}
-			</td>
-			<td class='text-center'>
-				{{ notes }}
-			</td>
-			<td class='text-center'>
-				<button data-id='{{ id }}' class='button-structure-edit btn btn-xs btn-primary'>Edit</button>
-				<button data-id='{{ id }}' class='button-structure-remove btn btn-xs btn-danger'>Remove</button>
-			</td>
-		</tr>
-	</script>
+<script id="template-structure-table-row" type="text/x-handlebars-template">?
+	<tr id='structure-{{ id }}' data-id='{{ id }}'>
+		<td class='text-center'>
+			{{ corporation_name }}
+		</td>
+		<td>
+			{{structureTypeName type_id}}
+		</td>
+		<td class='text-center'>
+			{{ created_at }}
+		</td>
+		<td class='text-center'>
+			{{ notes }}
+		</td>
+		<td class='text-center'>
+			<button data-id='{{ id }}' class='button-structure-edit btn btn-xs btn-primary'>Edit</button>
+			<button data-id='{{ id }}' class='button-structure-remove btn btn-xs btn-danger'>Remove</button>
+		</td>
+	</tr>
+</script>
