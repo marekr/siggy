6a2cf52e1310450771633e5ab87871466b3c4d3a
Marek Roszko <mark.roszko@gmail.com>
2017-02-27 01:29:45 -0500

    Upgrade handlebars, relocate files

--- js.thirdparty.bat
+++ js.thirdparty.bat
@@ -13,9 +13,9 @@ java -jar compiler.jar ^
 --js=jquery/jquery.jsPlumb-1.6.4.js ^
 --js=jquery/jquery.simplePagination.js ^
 --js=jquery/jquery.idle.js ^
---js=handlebars-v2.0.0.js ^
---js=handlebars.form-helpers.js ^
---js=handlebars.helpers.js ^
+--js=vendor/handlebars-v4.0.5.js ^
+--js=vendor/handlebars.form-helpers.js ^
+--js=vendor/handlebars.helpers.js ^
 --js=dropdown.js ^
 --js=tab.js ^
 --js=typeahead.bundle.js ^
