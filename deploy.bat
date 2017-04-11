28581294ba1078bdb9220422b098dfdef0dc6c20
Marek Roszko <mark.roszko@gmail.com>
2017-04-11 00:02:33 -0400

    Update deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,10 @@
+php artisan down
 git fetch --all
 git checkout --force origin/master
 
 git submodule sync
 git submodule update --init --recursive
 
-php artisan down
+composer install
 php artisan assets:compile
 php artisan up
