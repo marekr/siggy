be798240ae385adfeb8772c593433028dc9521b9
Marek Roszko <mark.roszko@gmail.com>
2017-04-30 02:40:26 -0400

    Fix artisan console.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,16 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+		\Siggy\Console\Commands\AssetsCompileCommand::class,
+		\Siggy\Console\Commands\GroupFixCodesCommand::class,
+		\Siggy\Console\Commands\EveSystemStatsCommand::class,
+		\Siggy\Console\Commands\SignaturesClearCommand::class,
+		\Siggy\Console\Commands\BillingChargeCommand::class,
+		\Siggy\Console\Commands\SessionsClearCommand::class,
+		\Siggy\Console\Commands\BillingPaymentsCommand::class,
+		\Siggy\Console\Commands\NotificationsClearCommand::class,
+		\Siggy\Console\Commands\CorporationsUpdateCommand::class,
+		\Siggy\Console\Commands\RedisCleanupCommand::class
     ];
 
     /**
@@ -24,8 +33,22 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+		$schedule->command('sessions:clear')
+		          ->hourlyAt(00);
+		$schedule->command('notifications:clear')
+		          ->daily();
+		$schedule->command('billing:payments')
+		          ->hourlyAt(05);
+		$schedule->command('billing:charge')
+		          ->daily();
+		$schedule->command('eve:systemstats')
+		          ->hourlyAt(01);
+		$schedule->command('signatures:clear')
+		          ->dailyAt('01:00');
+		$schedule->command('corps:update')
+		          ->dailyAt('02:00');
+		$schedule->command('redis:cleanup')
+		          ->dailyAt('03:00');
     }
 
     /**
