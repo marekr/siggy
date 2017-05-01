a24cc09bb3a27b719b2ec4cfda0a3526fd3dcec9
Marek Roszko <mark.roszko@gmail.com>
2017-04-30 21:33:36 -0400

    Remove last parts of Kohana.

--- application/config/auth.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-return array
-(
-	'sender_email_address' => 'noreply@borkedlabs.com'
-);
--- modules/restful-api/classes/Task/RestAPI.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Task_RestAPI extends Kohana_Task_RestAPI {}
