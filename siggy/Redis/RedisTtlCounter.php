378cce6155b0da42d1c4656a92ae0f001308f20f
Marek Roszko <mark.roszko@gmail.com>
2017-06-28 19:10:33 -0400

    Switch the RedisTtlCounter to laravel redis

--- siggy/Redis/RedisTtlCounter.php
+++ siggy/Redis/RedisTtlCounter.php
@@ -2,50 +2,34 @@
 
 namespace Siggy\Redis;
 
-use Siggy\Redis\Predis;
+use Illuminate\Support\Facades\Redis;
 
 class RedisTtlCounter
 {
 	private $queueName;
 	private $ttl = 3600;
-	private $predis = null;
 
 	public function __construct($queueName, $ttl = 3600)
 	{
-		$this->predis = Predis::client();
 		$this->queueName = $queueName;
 		$this->ttl = $ttl;
 	}
 
 	public function add($value)
 	{
-		if($this->predis == null)
-		{
-			return;
-		}
-
 		$value = serialize($value);
-		$this->predis->zadd($this->queueName, time(), $value);
+		
+		Redis::zadd($this->queueName, time(), $value);
 	}
 	
 	public function cleanup()
 	{
-		if($this->predis == null)
-		{
-			return;
-		}
-
-		$this->predis->zremrangebyscore($this->queueName, 0, (time() - $this->ttl));
+		Redis::zremrangebyscore($this->queueName, 0, (time() - $this->ttl));
 	}
 
 	public function count()
 	{
-		if($this->predis == null)
-		{
-			return;
-		}
-		
 		$this->cleanup();
-		return $this->predis->zcard($this->queueName);
+		return Redis::zcard($this->queueName);
 	}
 }
