a908303b5f626b02804ec2d48f9dacb44146f3d8
Marek Roszko <mark.roszko@gmail.com>
2017-07-22 19:45:06 -0400

    Bump version, adjust deploy script

--- deploy.bat
+++ deploy.bat
@@ -6,9 +6,12 @@ git submodule sync
 git submodule update --init --recursive
 
 call composer install --no-dev
+php artisan migrate
 php artisan assets:compile
+
+php artisan optimize
 php artisan route:cache
 php artisan api:cache
 php artisan config:cache
-php artisan optimize
+php artisan cache:clear
 php artisan up
