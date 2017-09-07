a0a6fbe4eb98daca2de9f28c9a06b49ee5f26a8c
Marek Roszko <mark.roszko@gmail.com>
2017-09-06 22:34:02 -0400

    Make scanned systems use the router for the system view button. Add notes activity page.

---
+++ resources/views/siggy/activities/notes.blade.php
@@ -0,0 +1,7 @@
+<div id="activity-notes" class="wrapper" style="display:none">
+	<p>
+		This page lists all systems that have signatures stored and the latest time they were entered as the Last Scan column.</a>
+	</p>
+	<br />
+	<br />
+</div>
