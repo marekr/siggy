648db7b72a0b2f5d50cf7c8fb1daf9ab6c1f1eb5
Marek Roszko <mark.roszko@gmail.com>
2018-01-21 17:39:42 -0500

    Update composer and clear config cache earlier in deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -5,12 +5,13 @@ git checkout --force origin/master
 call composer install --no-dev
 php artisan migrate
 
+php artisan config:cache
+
 call npm install
 php artisan assets:compile
 
 php artisan optimize
 php artisan route:cache
 php artisan api:cache
-php artisan config:cache
 php artisan cache:clear
 php artisan up
