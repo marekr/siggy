2842f48260fc1082109a191301e41bc14f3909d9
Marek Roszko <mark.roszko@gmail.com>
2018-01-23 02:24:10 -0500

    Update ravenjs path

--- resources/views/layouts/_javascript.blade.php
+++ resources/views/layouts/_javascript.blade.php
@@ -1,3 +1,3 @@
 @siggy_asset_js('vendor.js')
 @siggy_asset_js('siggy.js')
-<script src="https://cdn.ravenjs.com/3.14.0/raven.min.js"></script>
+<script src="https://cdn.ravenjs.com/3.22.1/raven.min.js"></script>
