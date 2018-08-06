b438ea6fae73bb3a8ad86c3ab0751c6864e2a421
Marek Roszko <mark.roszko@gmail.com>
2018-08-05 22:14:44 -0400

    Even more aggressive no caching

--- app/Http/Middleware/NoCache.php
+++ app/Http/Middleware/NoCache.php
@@ -11,6 +11,8 @@ class NoCache
         $response = $next($request);
 
         $response->header('Cache-Control', 'no-cache, no-store, must-revalidate');
+        $response->header('pragma', 'no-cache');
+        $response->header('expires', 'Mon, 28 Feb 1996 01:00:00 GMT');
 
         return $response;
     }
